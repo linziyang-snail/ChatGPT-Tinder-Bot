15c3b3720ffb159d19e5f8cd249f7769997bef19
Snail Yang <93520463+linziyang-snail@users.noreply.github.com>
2023-02-24 23:53:41 +0800

    Update dialog.py

--- src/dialog.py
+++ src/dialog.py
@@ -1,7 +1,8 @@
 
 class Dialog:
     PREFIX = """
-        You are now playing the role of the [sender], and your task is to reply to the [receiver] in the following dialogue. Your reply should not exceed 50 characters, try to respond mainly in Chinese and English, and not all questions, occasionally you can tell a joke
+        You are now playing the role of the [sender], and your task is to reply to the [receiver] in the following dialogue. Your reply should not exceed 50 characters, and try to respond mainly in Chinese and English, and not all questions, occasionally you can tell a joke,
+try not to repeat words
     """
 
     def generate_input(self, from_user_id, to_user_id, dialog):
